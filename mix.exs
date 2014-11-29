d67ad78ff4a28e1ab1ed36f7f1c715e555508d90
Andrea Leopardi <an.leopardi@gmail.com>
2014-11-29 16:03:46 +0100

    Fine-tune mix.exs for publishing on hex.pm

--- mix.exs
+++ mix.exs
@@ -1,25 +1,40 @@
 defmodule Convertat.Mixfile do
   use Mix.Project
 
+  @version "1.0.0"
+
   def project do
-    [app: :convertat,
-     version: "0.0.1",
-     name: "Convertat",
-     source_url: "https://github.com/whatyouhide/convertat",
-     elixir: "~> 1.0",
-     deps: deps]
+    [
+      app: :convertat,
+      version: @version,
+      name: "Convertat",
+      source_url: "https://github.com/whatyouhide/convertat",
+      elixir: "~> 1.0",
+      deps: deps,
+      package: package,
+      description: description
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  defp deps do
+  defp description do
+    """
+    Provides functions for converting from and to arbitrary bases.
+    """
+  end
+
+  defp package do
     [
-      {:ex_doc, "~> 0.6", only: :dev}
+      contributors: ["Andrea Leopardi"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/whatyouhide/convertat"},
     ]
+  end
+
+  defp deps do
+    [{:ex_doc, "~> 0.6", only: :dev}]
   end
 end
